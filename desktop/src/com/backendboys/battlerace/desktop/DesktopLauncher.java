18b6ce6879bd1c12cc9e9ce09601fe344f917399
FN890 <fn_890@icloud.com>
2020-09-07 16:46:00 +0200

    Project setup

---
+++ desktop/src/com/backendboys/battlerace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.backendboys.battlerace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.backendboys.battlerace.BattleRace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattleRace(), config);
+	}
+}
