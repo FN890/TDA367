6574057307e69bdfb7048a478e02f43098442230
Gustav Wadström <gustavwadstrom@Gustavs-MacBook-Air.local>
2020-09-08 11:23:48 +0200

    Screen Displaying

--- core/src/com/backendboys/battlerace/BattleRace.java
+++ core/src/com/backendboys/battlerace/BattleRace.java
@@ -1,20 +1,24 @@
 package com.backendboys.battlerace;
 
 import com.backendboys.battlerace.controller.GameController;
-import com.badlogic.gdx.ApplicationAdapter;
+import com.backendboys.battlerace.view.screens.MainMenu;
+import com.badlogic.gdx.Game;
 
-public class BattleRace extends ApplicationAdapter {
+public class BattleRace extends Game {
 
+    private Game game;
     private GameController gameController;
 
     @Override
     public void create() {
         gameController = new GameController();
+        this.game = this;
+        setScreen(new MainMenu());
     }
 
     @Override
     public void render() {
-
+        super.render();
     }
 
     @Override
--- core/src/com/backendboys/battlerace/view/screens/MainMenu.java
+++ core/src/com/backendboys/battlerace/view/screens/MainMenu.java
@@ -1,23 +1,41 @@
 package com.backendboys.battlerace.view.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainMenu extends AbstractScreen implements Screen {
 
     private SpriteBatch batch;
     private Stage stage;
+    private TextureAtlas textureAtlas;
+
+    private Map<String, Sprite> sprites = new HashMap<>();
 
     @Override
     public void show() {
         super.show();
+        //textureAtlas = new TextureAtlas("menusprites.txt");
         batch = new SpriteBatch();
+        stage = new Stage(getViewport(), batch);
+
+        //addSprites();
+    }
+
+    private void addSprites() {
+        Array<TextureAtlas.AtlasRegion> regions = textureAtlas.getRegions();
+
+        for (TextureAtlas.AtlasRegion region : regions) {
+            Sprite sprite = textureAtlas.createSprite(region.name);
+
+            sprites.put(region.name, sprite);
+        }
     }
 
     @Override
@@ -30,7 +48,8 @@ public class MainMenu extends AbstractScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        //batch.setProjectionMatrix(camera.combined);
+        super.resize(width, height);
+        batch.setProjectionMatrix(getCamera().combined);
     }
 
     @Override
@@ -50,6 +69,7 @@ public class MainMenu extends AbstractScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        textureAtlas.dispose();
+        sprites.clear();
     }
 }
