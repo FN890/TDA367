a77eb7cf1cabd505374d384af696b76a1fe8fec8
Gustav Wadström <gustavwadstrom@Gustavs-MacBook-Air.local>
2020-09-08 09:50:19 +0200

    Created AbstractScreen

---
+++ core/src/com/backendboys/battlerace/view/screens/AbstractScreen.java
@@ -0,0 +1,51 @@
+package com.backendboys.battlerace.view.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public abstract class AbstractScreen implements Screen {
+
+    private Viewport viewport;
+    private OrthographicCamera camera;
+
+    @Override
+    public void show() {
+        camera = new OrthographicCamera();
+        viewport = new ExtendViewport(800, 600, camera);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.57f, 0.77f, 0.85f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
