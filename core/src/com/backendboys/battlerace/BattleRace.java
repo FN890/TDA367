da6da56f3ea58a18cecdffc81eb2f98b01ee897d
Bamsefar <bojkat@hotmail.com>
2020-09-08 11:34:33 +0200

    Added music

--- core/src/com/backendboys/battlerace/BattleRace.java
+++ core/src/com/backendboys/battlerace/BattleRace.java
@@ -3,6 +3,8 @@ package com.backendboys.battlerace;
 import com.backendboys.battlerace.controller.GameController;
 import com.backendboys.battlerace.view.screens.MainMenu;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 
 public class BattleRace extends Game {
 
@@ -12,6 +14,9 @@ public class BattleRace extends Game {
     @Override
     public void create() {
         gameController = new GameController();
+        Music music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        music.play();
+        music.setLooping(true);
         this.game = this;
         setScreen(new MainMenu());
     }
