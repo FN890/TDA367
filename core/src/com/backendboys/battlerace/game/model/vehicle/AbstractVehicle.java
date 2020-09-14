c59d8e38e1bb7393e1416eded7832ea1f2d4b155
Gustav Wadström <gustav.wadstrm@gmail.com>
2020-09-15 01:12:03 +0200

    Abstracted Car to AbstractVehicle

--- core/src/com/backendboys/battlerace/game/model/vehicle/AbstractVehicle.java
+++ core/src/com/backendboys/battlerace/game/model/vehicle/AbstractVehicle.java
@@ -1,16 +1,88 @@
 package com.backendboys.battlerace.game.model.vehicle;
 
-import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
-public abstract class AbstractVehicle {
+abstract class AbstractVehicle {
 
-    private final Body body;
+    private static final float FRICTION = 0.5f;
+    private static final float RESTITUTION = 0.6f;
 
-    protected AbstractVehicle() {
+    private Body body;
 
+    private final float topSpeed;
+    private final float acceleration;
+    private final float angularAcceleration;
 
+    protected AbstractVehicle(World world, float mass, float posX, float posY, float topSpeed, float acceleration, float angularAcceleration) {
+        this.topSpeed = topSpeed;
+        this.acceleration = acceleration;
+        this.angularAcceleration = angularAcceleration;
+
+        instantiateBody(world, mass, posX, posY);
     }
 
+    private void instantiateBody(World world, float mass, float posX, float posY) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(posX, posY);
 
+        Body body = world.createBody(bodyDef);
+
+        Shape shape = createVehicleShape();
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = createVehicleShape();
+        fixtureDef.density = (float) (mass/(Math.pow(2, shape.getRadius())*Math.PI));
+        fixtureDef.friction = FRICTION;
+        fixtureDef.restitution = RESTITUTION;
+
+        body.createFixture(fixtureDef);
+        shape.dispose();
+
+        this.body = body;
+    }
+
+    protected abstract Shape createVehicleShape();
+
+    public void gas() {
+        if (body.getLinearVelocity().x > topSpeed) {
+            return;
+        }
+        float force = acceleration * body.getMass();
+        body.applyForceToCenter(force, 0f, true);
+    }
+
+    public void brake() {
+        if (body.getLinearVelocity().x < 0) {
+            return;
+        }
+        float force = -acceleration * body.getMass() * 0.5f;
+        body.applyForceToCenter(force, 0f, true);
+    }
+
+    public void rotateRight() {
+        if (body.getAngularVelocity() < -1 * 2*Math.PI) {
+            return;
+        }
+        float torque = -angularAcceleration * body.getMassData().I;
+        body.applyTorque(torque, true);
+    }
+
+    public void rotateLeft() {
+        if (body.getAngularVelocity() > 1 * 2 * Math.PI) {
+            return;
+        }
+        float torque = angularAcceleration * body.getMassData().I;
+        body.applyTorque(torque, true);
+    }
+
+    public Vector2 getPos() {
+        return new Vector2(body.getPosition());
+    }
+
+    public void setPos(Vector2 pos) {
+        body.getPosition().set(pos);
+    }
 
 }
--- core/src/com/backendboys/battlerace/game/model/vehicle/Car.java
+++ core/src/com/backendboys/battlerace/game/model/vehicle/Car.java
@@ -3,82 +3,16 @@ package com.backendboys.battlerace.game.model.vehicle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
-public class Car {
+class Car extends AbstractVehicle {
 
-    private final Body body;
-    private final float topSpeed;
-    private final float acceleration;
-    private final float mass;
-    private final float angularAcceleration;
-
-    public Car(World world, float posX, float posY) {
-
-        topSpeed = 200;
-        acceleration = 500;
-        mass = 1000;
-        angularAcceleration = (float) (3 * Math.PI * 2);
-
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set(posX, posY);
-
-        Body body = world.createBody(bodyDef);
-
-        Shape shape = new CircleShape();
-        shape.setRadius(5);
-
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.density = (float) (1000f/(shape.getRadius()*shape.getRadius() * Math.PI));
-        fixtureDef.friction = 0.4f;
-        fixtureDef.restitution = 0.6f;
-
-        body.createFixture(fixtureDef);
-        shape.dispose();
-
-        this.body = body;
-        System.out.println(body.getMass());
+    //TODO: Implement as Basic car, should be able to have subclasses: Sportscar etc.
+    protected Car(World world, float mass, float posX, float posY, float topSpeed, float acceleration, float angularAcceleration) {
+        super(world, mass, posX, posY, topSpeed, acceleration, angularAcceleration);
     }
 
-    public void gas() {
-        if (body.getLinearVelocity().x > topSpeed) {
-            return;
-        }
-        float force = acceleration * body.getMass();
-        body.applyForceToCenter(force, 0f, true);
-        System.out.println(body.getLinearVelocity().x);
-    }
-
-    public void brake() {
-        if (body.getLinearVelocity().x < 0) {
-            return;
-        }
-        float force = -acceleration * body.getMass() * 0.5f;
-        body.applyForceToCenter(force, 0f, true);
-    }
-
-    public void rotateRight() {
-        if (body.getAngularVelocity() < -1 * 2*Math.PI) {
-            return;
-        }
-        float torque = -angularAcceleration * body.getMassData().I;
-        body.applyTorque(torque, true);
-    }
-
-    public void rotateLeft() {
-        if (body.getAngularVelocity() > 1 * 2 * Math.PI) {
-            return;
-        }
-        float torque = angularAcceleration * body.getMassData().I;
-        body.applyTorque(torque, true);
-    }
-
-    public Vector2 getPos() {
-        return new Vector2(body.getPosition());
-    }
-
-    public void setPos(Vector2 pos) {
-        body.getPosition().set(pos);
+    @Override
+    protected Shape createVehicleShape() {
+        return null;
     }
 
 }
