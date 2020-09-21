05e71e865fe30ddeb170eee8e4b75772be07753d
Simon Andersson <xerxz5115@gmail.com>
2020-09-21 18:04:58 +0200

    Removed random change GroundGenerator

--- Game/core/src/com/backendboys/battlerace/model/gamemodel/world/GroundGenerator.java
+++ Game/core/src/com/backendboys/battlerace/model/gamemodel/world/GroundGenerator.java
@@ -60,9 +60,9 @@ public class GroundGenerator {
 
     private void generateVertices() {
         float xPos = 0, yPos = 0;
-        Random r = new Random();
+
         for (int i = 0; i < numberVertices; i++) {
-            yPos = (30 * (float) Math.sin(xPos * 0.01)) + (float) (r.nextInt(20)*Math.sin(xPos * 0.1)) + 30;
+            yPos = 30 * (float) Math.sin(xPos * 0.01) + 30;
             xPos += step;
             vertices.add(new Vector2(xPos, yPos + 10));
         }
