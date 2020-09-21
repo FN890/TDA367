7e3084e25b3e2953504c5297648cc63c1f79557a
Simon Andersson <xerxz5115@gmail.com>
2020-09-21 18:48:39 +0200

    Added test to PlayerTest

--- Game/core/src/test/PlayerTest.java
+++ Game/core/src/test/PlayerTest.java
@@ -1,23 +1,55 @@
 
 import com.backendboys.battlerace.model.gamemodel.player.Player;
+import com.backendboys.battlerace.model.gamemodel.powerups.MissilePowerUp;
+import com.backendboys.battlerace.model.gamemodel.powerups.NitroPowerUp;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    Player player;
+    private final String testName = "Dummy";
+
+    public PlayerTest(){
+        player = new Player(testName);
+    }
+
     @Test
     public void PlayerCreated(){
-        assertNotNull(new Player("name"));
+        assertNotNull(player);
     }
 
     @Test
     public void PlayerCheckName(){
-        Player player = new Player("Player1");
+        assertEquals(testName, player.getName());
     }
 
     @Test
+    public void PlayerAddVehicle(){
+        //Not sure how to implement, maybe create a dummy world because we need
+        // a world to create a vehicle
+    }
+
+    /**
+     * PowerUps not implemented fully yet
+     */
+    @Test
     public void PlayerAddPowerUp(){
-        Player player = new Player("name");
+        //player.addPowerUp(new MissilePowerUp());
+        //player.addPowerUp(new NitroPowerUp());
+
+        //assertTrue(player.getListPowerUp().size() == 2);
+    }
+
+    /**
+     * PowerUps not implemented fully yet
+     */
+    @Test
+    public void PlayerUsePowerUp(){
+        //player.usePowerUp();
+        //player.usePowerUp();
+
+        //assertTrue(player.getListPowerUp().size() == 0);
     }
 }
