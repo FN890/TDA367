70dff098528515a934cce37467ab7edaa6c02175
Simon Andersson <xerxz5115@gmail.com>
2020-09-24 13:27:35 +0200

    Removed double reference from BattleRace to Controllers

--- Game/core/src/com/backendboys/battlerace/BattleRace.java
+++ Game/core/src/com/backendboys/battlerace/BattleRace.java
@@ -8,20 +8,17 @@ import com.badlogic.gdx.graphics.g2d.Gdx2DPixmap;
 
 public class BattleRace extends Game {
 
-    private MenuController menuController;
-    private GameController gameController;
-
     @Override
     public void create() {
-        menuController = new MenuController(this);
+        MenuController menuController = new MenuController(this);
     }
-
+    
     public void startMenu() {
-        menuController = new MenuController(this);
+        MenuController menuController = new MenuController(this);
     }
 
     public void startSinglePlayer(){
-        gameController = new GameController(this);
+        GameController gameController = new GameController(this);
         Gdx.input.setInputProcessor(gameController);
     }
 
