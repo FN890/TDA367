6f1160632ba44d52cc684c3de9ee36a32411b58d
Gustav Wadström <gustav.wadstrm@gmail.com>
2020-09-28 11:46:04 +0200

    Implemented UDPServer

---
+++ Server/src/main/java/server/UDPServer.java
@@ -0,0 +1,43 @@
+package server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class UDPServer implements Runnable {
+
+    private final int port;
+    private final DatagramSocket socket;
+
+    public UDPServer(int port) throws SocketException {
+        this.port = port;
+        this.socket = new DatagramSocket(port);
+    }
+
+    @Override
+    public void run() {
+        byte[] buffer = new byte[548];
+
+        try {
+            while (true) {
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                socket.receive(request);
+
+                String message = new String(request.getData(), 0, request.getLength());
+                System.out.println("Packet received: " + message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendPacket(String message, InetAddress address, int port) throws IOException {
+        byte[] byteMsg =  message.getBytes();
+        
+        DatagramPacket packet = new DatagramPacket(byteMsg, 0, byteMsg.length, address, port);
+        socket.send(packet);
+    }
+
+}
