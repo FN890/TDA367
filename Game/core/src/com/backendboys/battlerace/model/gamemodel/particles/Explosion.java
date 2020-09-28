436db96955ec327f56d0a51ba51d772ce0aaa312
RasOtte <Rasmus.otterlind@gmail.com>
2020-09-28 11:05:02 +0200

    Added javaDocs in particles package

--- Game/core/src/com/backendboys/battlerace/model/gamemodel/particles/Explosion.java
+++ Game/core/src/com/backendboys/battlerace/model/gamemodel/particles/Explosion.java
@@ -6,9 +6,18 @@ import com.badlogic.gdx.physics.box2d.World;
 
 import java.util.ArrayList;
 
+/**
+ * A simple class that when created generates an explosion with a set number of particles
+ * Uses the ExplosionParticle class to create particles for the explosion
+ */
 public class Explosion {
 
-
+    /**
+     *
+     * @param pos The postion for the exlposion
+     * @param numParticles the number off particles that are set the be generated
+     * @param world The world in which the explosion occurs
+     */
     public Explosion(Vector2 pos, int numParticles, World world) {
         for (int i = 0; i < numParticles; i++) {
             float angle = (i / (float) numParticles) * 360 * MathUtils.degreesToRadians;
--- Game/core/src/com/backendboys/battlerace/model/gamemodel/particles/ExplosionParticle.java
+++ Game/core/src/com/backendboys/battlerace/model/gamemodel/particles/ExplosionParticle.java
@@ -3,19 +3,30 @@ package com.backendboys.battlerace.model.gamemodel.particles;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
+/**
+ * A class that is used to create particles for explosions one instance is one particle
+ */
 public class ExplosionParticle {
+
     private int movementPower = 100;
     private Body body;
 
-    public ExplosionParticle(World world, Vector2 vector, Vector2 rayDir) {
+    /**
+     * Constructor for a particle
+     * Adds a particle to the world
+     * @param world The world where the particle is created
+     * @param pos used to determine the position of the particle
+     * @param rayDir Used to determine which direction in 2d space the particle is travelling
+     */
+    public ExplosionParticle(World world, Vector2 pos, Vector2 rayDir) {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
         bodyDef.fixedRotation = true;
         bodyDef.bullet = true;
         bodyDef.linearDamping = 0.3f; //air resistance
         bodyDef.gravityScale = 0;
-        bodyDef.position.x = vector.x;
-        bodyDef.position.y = vector.y;
+        bodyDef.position.x = pos.x;
+        bodyDef.position.y = pos.y;
         rayDir.scl(movementPower);
         bodyDef.linearVelocity.x = rayDir.x;
         bodyDef.linearVelocity.y = rayDir.y;
