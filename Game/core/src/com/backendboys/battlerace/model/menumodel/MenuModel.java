a3352e0e645674754d43b01154892a7d920f0dfc
RasOtte <Rasmus.otterlind@gmail.com>
2020-09-30 09:19:46 +0200

    added javaDocs to MenuModel

--- Game/core/src/com/backendboys/battlerace/model/menumodel/MenuModel.java
+++ Game/core/src/com/backendboys/battlerace/model/menumodel/MenuModel.java
@@ -1,7 +1,9 @@
 package com.backendboys.battlerace.model.menumodel;
 
 
-//Menu model, store menu settings.
+/**
+ * Not implemented yet but is supposed to be used to store menu settings
+ */
 public class MenuModel {
 
 }
