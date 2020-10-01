b5ea205edf5b46637a7900bd44d361763e462041
Simon Andersson <xerxz5115@gmail.com>
2020-10-01 21:22:28 +0200

    Added javadoc to IModelListener

--- Game/core/src/com/backendboys/battlerace/model/gamemodel/IModelListener.java
+++ Game/core/src/com/backendboys/battlerace/model/gamemodel/IModelListener.java
@@ -1,5 +1,8 @@
 package com.backendboys.battlerace.model.gamemodel;
 
+/**
+ *  Not used atm but can be used for notifying listeners when model is updated
+ */
 public interface IModelListener {
 
     void update();
