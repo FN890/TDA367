53c500072e3df5e372e575177497e336331bb052
RasOtte <Rasmus.otterlind@gmail.com>
2020-10-04 11:34:26 +0200

    added javaDocs to AbstractExplosive

--- Game/core/src/com/backendboys/battlerace/model/gamemodel/particles/AbstractExplosive.java
+++ Game/core/src/com/backendboys/battlerace/model/gamemodel/particles/AbstractExplosive.java
@@ -1,7 +1,14 @@
 package com.backendboys.battlerace.model.gamemodel.particles;
 
+/**
+ * A abstract class used to identify
+ * Missiles and ExplosionParticles when collisions happen
+ */
 public abstract class AbstractExplosive {
-
+    /**
+     * The method that is called when explosives Collide
+     * Called from the collisionListener
+     */
     public abstract void explosiveCollided();
 
     private boolean toBeRemoved = false;
