f39183b509fc67e7311f9048115313498da9e222
Simon Andersson <xerxz5115@gmail.com>
2020-10-06 11:06:57 +0200

    Created protocols

--- Game/core/src/com/backendboys/battlerace/services/GameClient.java
+++ Game/core/src/com/backendboys/battlerace/services/GameClient.java
@@ -1,9 +1,31 @@
 package com.backendboys.battlerace.services;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class GameClient implements Runnable{
+
+    private String hostname;
+    private int port;
+
+    private PrintWriter writer;
+    private BufferedReader reader;
+
+    public GameClient(String hostname, int port){
+        this.hostname = hostname;
+        this.port = port;
+    }
 
     @Override
     public void run() {
+        try{
+            Socket socket = new Socket(hostname, port);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
