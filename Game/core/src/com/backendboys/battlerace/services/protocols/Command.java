58a6fc284f5526032574b1d23d445391cc20b2e1
Simon Andersson <xerxz5115@gmail.com>
2020-10-06 11:08:01 +0200

    Created Command

---
+++ Game/core/src/com/backendboys/battlerace/services/protocols/Command.java
@@ -0,0 +1,23 @@
+package com.backendboys.battlerace.services.protocols;
+
+class Command implements ICommand {
+
+    private final String cmd;
+    private final String[] args;
+
+    Command(String cmd, String[] args) {
+        this.cmd = cmd;
+        this.args = args;
+    }
+
+    @Override
+    public String getCmd() {
+        return cmd;
+    }
+
+    @Override
+    public String[] getArgs() {
+        return args;
+    }
+}
+
