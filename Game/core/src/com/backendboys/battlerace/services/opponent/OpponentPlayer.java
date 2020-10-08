de2caa2e996b4e35d9499af7020c528bd051f487
Simon Andersson <xerxz5115@gmail.com>
2020-10-08 19:00:34 +0200

    Updated OpponentPlayer

--- Game/core/src/com/backendboys/battlerace/services/opponent/OpponentPlayer.java
+++ Game/core/src/com/backendboys/battlerace/services/opponent/OpponentPlayer.java
@@ -1,4 +1,14 @@
 package com.backendboys.battlerace.services.opponent;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class OpponentPlayer {
+
+    private String playerName;
+    private Vector2 playerPosition;
+
+    public OpponentPlayer(String playerName, Vector2 playerPosition){
+        this.playerName = playerName;
+        this.playerPosition = playerPosition;
+    }
 }
