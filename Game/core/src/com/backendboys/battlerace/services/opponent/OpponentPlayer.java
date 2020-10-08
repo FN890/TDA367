143d2762bb5bee3436b2f469b552eac2e189a554
Simon Andersson <xerxz5115@gmail.com>
2020-10-08 19:14:36 +0200

    Added getters OpponentPlayer

--- Game/core/src/com/backendboys/battlerace/services/opponent/OpponentPlayer.java
+++ Game/core/src/com/backendboys/battlerace/services/opponent/OpponentPlayer.java
@@ -6,9 +6,23 @@ public class OpponentPlayer {
 
     private String playerName;
     private Vector2 playerPosition;
+    private float playerRotation;
 
-    public OpponentPlayer(String playerName, Vector2 playerPosition){
+    public OpponentPlayer(String playerName, Vector2 playerPosition, float playerRotation){
         this.playerName = playerName;
         this.playerPosition = playerPosition;
+        this.playerRotation = playerRotation;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public Vector2 getPlayerPosition() {
+        return playerPosition;
+    }
+
+    public float getPlayerRotation() {
+        return playerRotation;
     }
 }
