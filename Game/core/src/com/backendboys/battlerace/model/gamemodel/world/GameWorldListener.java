cd6b002fc41097c7e75e8285dcd34811ab02011e
FN890 <fn_890@icloud.com>
2020-10-10 17:47:01 +0200

    Changed Javadoc

--- Game/core/src/com/backendboys/battlerace/model/gamemodel/world/GameWorldListener.java
+++ Game/core/src/com/backendboys/battlerace/model/gamemodel/world/GameWorldListener.java
@@ -3,7 +3,7 @@ package com.backendboys.battlerace.model.gamemodel.world;
 public interface GameWorldListener {
 
     /**
-     * Notify listeners that gameWorld.stepWorld() has been used.
+     * Notify listeners that gameWorld.stepWorld() has been called.
      */
     void onGameWorldStepped();
 
