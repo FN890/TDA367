d3d4a36dfe8b5a23d08d810be50b03dd0d5cbc33
RasOtte <Rasmus.otterlind@gmail.com>
2020-10-12 10:20:31 +0200

    added render method

--- Game/core/src/com/backendboys/battlerace/view/game/ExplosionParticleRender.java
+++ Game/core/src/com/backendboys/battlerace/view/game/ExplosionParticleRender.java
@@ -1,10 +1,13 @@
 package com.backendboys.battlerace.view.game;
 
+import com.backendboys.battlerace.model.gamemodel.particles.IParticle;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.ArrayList;
 
 public class ExplosionParticleRender {
 
@@ -23,5 +26,26 @@ public class ExplosionParticleRender {
         sprite = new Sprite(texture);
         sprite.setSize(WIDTH, HEIGHT);
         sprite.setOrigin(WIDTH / 2f, HEIGHT / 2f);
+    }
+
+    public void render(ArrayList<IParticle> particles) {
+        batch.begin();
+        for (IParticle particle : particles) {
+            if (withinCamera(particle)) {
+
+                sprite.setPosition(particle.getPosition().x, particle.getPosition().y);
+
+                batch.setProjectionMatrix(orthographicCamera.combined);
+                sprite.draw(batch);
+            }
+        }
+        batch.end();
+    }
+
+    private boolean withinCamera(IParticle particle) {
+        if (particle.getPosition().x > orthographicCamera.position.x - orthographicCamera.viewportWidth) {
+            return particle.getPosition().x < orthographicCamera.position.x + orthographicCamera.viewportWidth;
+        }
+        return true;
     }
 }
