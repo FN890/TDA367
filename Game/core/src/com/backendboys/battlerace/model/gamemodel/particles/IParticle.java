57bc6fee05b9129f8cec9e6106c784abe4c859ce
RasOtte <Rasmus.otterlind@gmail.com>
2020-10-15 23:06:17 +0200

    added javaDoc to iParticle

--- Game/core/src/com/backendboys/battlerace/model/gamemodel/particles/IParticle.java
+++ Game/core/src/com/backendboys/battlerace/model/gamemodel/particles/IParticle.java
@@ -3,6 +3,10 @@ package com.backendboys.battlerace.model.gamemodel.particles;
 
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * an interface used to identify particles and missiles
+ * also used when getting the info for rendering
+ */
 public interface IParticle {
 
     Vector2 getPosition();
