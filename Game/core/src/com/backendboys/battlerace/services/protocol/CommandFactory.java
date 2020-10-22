3cc55a514da820b78e008f6bb2b4f53014751329
Bamsefar <bojkat@hotmail.com>
2020-10-22 18:51:55 +0200

    Made CommandFactory constructor private

--- Game/core/src/com/backendboys/battlerace/services/protocol/CommandFactory.java
+++ Game/core/src/com/backendboys/battlerace/services/protocol/CommandFactory.java
@@ -2,6 +2,9 @@ package com.backendboys.battlerace.services.protocol;
 
 public class CommandFactory {
 
+    private CommandFactory() {
+    }
+
     public static ICommand createCommand(String cmd, String... args) {
         return new Command(cmd, args);
     }
