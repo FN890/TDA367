163227916aadc7ce19218ef7c9ed645f7ec0e651
Bamsefar <bojkat@hotmail.com>
2020-10-22 18:26:05 +0200

    Added JavaDoc to IPowerUp

--- Game/core/src/com/backendboys/battlerace/model/gamemodel/powerups/IPowerUp.java
+++ Game/core/src/com/backendboys/battlerace/model/gamemodel/powerups/IPowerUp.java
@@ -4,12 +4,29 @@ import com.backendboys.battlerace.model.gamemodel.player.Player;
 import com.backendboys.battlerace.model.gamemodel.world.GameWorldListener;
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * Interface for power ups.
+ * This interface is used in all classes that uses power ups in some way.
+ */
 public interface IPowerUp extends GameWorldListener {
 
+    /**
+     * Removes the power up from the world, called when picked up by a player.
+     */
     void remove();
 
+    /**
+     * The logic for what happens when a player uses a power up.
+     *
+     * @param player The player who used the power up.
+     */
     void use(Player player);
 
+    /**
+     * Getter for the position of the power up in the world.
+     *
+     * @return The position in the world in the form a {@link Vector2}.
+     */
     Vector2 getPosition();
 
 }
