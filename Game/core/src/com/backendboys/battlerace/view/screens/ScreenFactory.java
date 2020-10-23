d1f62cf200fafd5e7ce5f9319b0a6ab40bba2a01
Bamsefar <bojkat@hotmail.com>
2020-10-23 11:54:00 +0200

    JavaDoc to ScreenFactory

--- Game/core/src/com/backendboys/battlerace/view/screens/ScreenFactory.java
+++ Game/core/src/com/backendboys/battlerace/view/screens/ScreenFactory.java
@@ -12,18 +12,38 @@ public class ScreenFactory {
     private ScreenFactory() {
     }
 
+    /**
+     * Creates a {@link MainMenu} Screen.
+     *
+     * @return The screen in the form of {@link IScreen}
+     */
     public static IScreen createMainMenu(MenuController menuController) {
         return new MainMenu(menuController);
     }
 
+    /**
+     * Creates a {@link OptionsMenu} Screen.
+     *
+     * @return The screen in the form of {@link IScreen}
+     */
     public static IScreen createOptionsMenu(MenuController menuController) {
         return new OptionsMenu(menuController);
     }
 
+    /**
+     * Creates a {@link GameScreen} Screen.
+     *
+     * @return The screen in the form of {@link IScreen}
+     */
     public static IGameScreen createGameScreen(GameController gameController, boolean debugMode) {
         return new GameScreen(gameController, debugMode);
     }
 
+    /**
+     * Creates a {@link MultiplayerMenu} Screen.
+     *
+     * @return The screen in the form of {@link IScreen}
+     */
     public static IScreen createMultiplayerMenu(MenuController menuController) {
         return new MultiplayerMenu(menuController);
     }
