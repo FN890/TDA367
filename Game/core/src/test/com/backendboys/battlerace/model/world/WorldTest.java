c948d24f0923d23d3f86c1fc0789ad7a1c830532
FN890 <fn_890@icloud.com>
2020-10-23 16:55:02 +0200

    Created test for GroundGenerator

---
+++ Game/core/src/test/com/backendboys/battlerace/model/world/WorldTest.java
@@ -0,0 +1,32 @@
+package com.backendboys.battlerace.model.world;
+
+import com.backendboys.battlerace.model.world.ground.GroundStrategyFactory;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WorldTest {
+
+    @Test
+    public void verticesTest() {
+        Random random = new Random();
+        int verticesAmount = random.nextInt(1000);
+        GameWorld gameWorld = new GameWorld(GroundStrategyFactory.getSinCosStrategy(verticesAmount, 60, 5), 1);
+        assertEquals(gameWorld.getGroundVertices().size(), verticesAmount);
+    }
+
+    @Test
+    public void groundGeneratorTest() {
+        Random random = new Random();
+        int verticesAmount = random.nextInt(1000);
+        World world = new World(new Vector2(0, -10), true);
+        GroundGenerator groundGenerator = new GroundGenerator();
+        groundGenerator.generateGround(world, GroundStrategyFactory.getSinCosStrategy(verticesAmount, 1000, 5), 1);
+    }
+
+
+}
