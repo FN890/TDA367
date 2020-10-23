89057f3a95e2ef2917c70968b3791c7df7f72800
FN890 <fn_890@icloud.com>
2020-10-23 10:54:26 +0200

    Added Javadoc

--- Game/core/src/com/backendboys/battlerace/model/vehicle/ICar.java
+++ Game/core/src/com/backendboys/battlerace/model/vehicle/ICar.java
@@ -4,11 +4,27 @@ import com.badlogic.gdx.math.Vector2;
 
 public interface ICar extends IVehicle {
 
+    /**
+     * Returns the position of the front wheel.
+     * @return the position.
+     */
     Vector2 getFrontWheelPosition();
 
+    /**
+     * Returns the position of the rear wheel.
+     * @return the position.
+     */
     Vector2 getRearWheelPosition();
 
+    /**
+     * Returns the rotation of the front wheel.
+     * @return the rotation.
+     */
     float getFrontWheelRotation();
 
+    /**
+     * Returns the rotation of the rear wheel.
+     * @return the rotation.
+     */
     float getRearWheelRotation();
 }
