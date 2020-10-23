61e18bd81ff97bd742787a3c73c88386a222b79f
Bamsefar <bojkat@hotmail.com>
2020-10-23 11:57:07 +0200

    JavaDoc IGameScreen

--- Game/core/src/com/backendboys/battlerace/view/screens/IGameScreen.java
+++ Game/core/src/com/backendboys/battlerace/view/screens/IGameScreen.java
@@ -2,7 +2,13 @@ package com.backendboys.battlerace.view.screens;
 
 import com.backendboys.battlerace.controller.ServerController;
 
+/**
+ * Interface for GameScreen, used for encapsulation.
+ */
 public interface IGameScreen extends IScreen {
 
+    /**
+     * Sets the {@link ServerController}.
+     */
     void setServerController(ServerController serverController);
 }
