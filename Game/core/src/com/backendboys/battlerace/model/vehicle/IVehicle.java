63f0b64335dbb5950d98d9a205346a82e7103bfe
Bamsefar <bojkat@hotmail.com>
2020-10-23 11:49:34 +0200

    Added JavaDoc to IVehicle

--- Game/core/src/com/backendboys/battlerace/model/vehicle/IVehicle.java
+++ Game/core/src/com/backendboys/battlerace/model/vehicle/IVehicle.java
@@ -8,23 +8,66 @@ import com.badlogic.gdx.physics.box2d.World;
  */
 public interface IVehicle {
 
+    /**
+     * Method for when car is accelerating.
+     */
     void gas();
 
+    /**
+     * Method for when car is braking.
+     */
     void brake();
 
+    /**
+     * Method for rotating the car clockwise.
+     */
     void rotateRight();
 
+    /**
+     * Method for rotating the car counter-clockwise.
+     */
     void rotateLeft();
 
+    /**
+     * Gets the position of the car in world.
+     *
+     * @return Position in the form of {@link Vector2}.
+     */
     Vector2 getPosition();
 
+    /**
+     * Gets the rotation of the car.
+     *
+     * @return The rotation in the form of a float in radians.
+     */
     float getRotation();
 
+    /**
+     * Sets the acceleration of the car.
+     * Used for boosting it's acceleration when using {@link com.backendboys.battlerace.model.powerups.NitroPowerUp}.
+     *
+     * @param acceleration The new acceleration in the form of a float.
+     */
     void setAcceleration(float acceleration);
 
+    /**
+     * Gets the acceleration of the car.
+     *
+     * @return The acceleration in the form of a float.
+     */
     float getAcceleration();
 
+    /**
+     * Gets the world the car is currently in.
+     *
+     * @return The {@link World} in which the car is in.
+     */
     World getWorld();
 
+    /**
+     * Gets the linear velocity of the car.
+     *
+     * @return The linear velocity in the form of a {@link Vector2}.
+     */
     Vector2 getLinearVelocity();
 }
